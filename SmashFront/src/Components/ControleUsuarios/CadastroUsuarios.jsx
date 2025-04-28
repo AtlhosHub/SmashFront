a6de673e855cb48ed87e27d61aa3914ca60d92fd
Gouvea <cauagouveanascimento@gmail.com>
2025-04-27 21:05:14 -0300

    feat: ajuste no layout

--- SmashFront/src/Components/ControleUsuarios/CadastroUsuarios.jsx
+++ SmashFront/src/Components/ControleUsuarios/CadastroUsuarios.jsx
@@ -1,51 +1,43 @@
-import { DefaultHeader } from "../DefaultComponents/DefaultHeader/DefaultHeader"
-import { DefaultBreadcrumb } from "../DefaultComponents/DefaultBreadcrumb/DefaultBreadcrumb"
+import { DefaultHeader } from "../DefaultComponents/DefaultHeader/DefaultHeader";
+import { DefaultBreadcrumb } from "../DefaultComponents/DefaultBreadcrumb/DefaultBreadcrumb";
 import { Box } from "@mui/material";
 import { MenuCadastro } from "../FichaInscricao/Components/MenuCadastro/MenuCadastro";
 import { FormInfoUsuario } from "./Components/FormularioCadastroUsuario/FormInfoUsuario";
 
 export const CadastroUsuarios = () => {
-
     const rotas = [
         {
             route: "/controleUsuarios",
-            description: "Controle de Usuários"
+            description: "Controle de Usuários",
         },
         {
             route: "/cadastroUsuarios",
-            description: "Cadastrar Usuário"
-        }
-    ]
+            description: "Cadastrar Usuário",
+        },
+    ];
 
     return (
         <>
-            <Box>
-                <DefaultHeader pageTitle="Cadastrar Usuário" />
-                <DefaultBreadcrumb rotas={rotas} />
-
-                <Box sx={{
+            <Box
+                sx={{
                     display: "grid",
                     gridTemplateRows: "auto auto 1fr",
                     height: "100vh",
+                }}
+            >
+                <DefaultHeader pageTitle="Cadastrar Usuário" />
+                <DefaultBreadcrumb rotas={rotas} />
 
-                }}>
-
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "row",
-                            flexGrow: 1,
-                        }}
-                    >
-                        <MenuCadastro
-
-                        />
-                        <FormInfoUsuario />
-                    </Box>
-
-
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                    }}
+                >
+                    <MenuCadastro />
+                    <FormInfoUsuario />
                 </Box>
             </Box>
         </>
-    )
-}
+    );
+};
